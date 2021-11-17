99f2ba5dbcf44e0d10442cda3e55857a8492e172
Ashraful-Mijan <ashrafulmijan5@gmail.com>
2021-11-17 10:24:50 +0600

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import Posts from './components/Posts';
-// import Pagination from './components/Pagination';
-import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
+import Pagination from './components/Pagination';
+// import Pagination from '@mui/material/Pagination';
+// import Stack from '@mui/material/Stack';
 
 export default function App() {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(5);
+  const [postsPerPage, setPostsPerPage] = useState(10);
   useEffect(() => {
     const fetchPosts = async () => {
       setLoading(true);
@@ -22,7 +22,7 @@ export default function App() {
     fetchPosts();
   }, []);
 
-  //Get fcurrent posts
+  //Get current posts
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
@@ -34,15 +34,15 @@ export default function App() {
       <h1 className="text-center text-primary mb-3">React Pagination!</h1>
 
       <Posts posts={currentPosts} loading={loading} />
-      {/* <Pagination
+      <Pagination
         postsPerPage={postsPerPage}
         totalPosts={posts.length}
         paginate={paginate}
-      /> */}
-      <Stack spacing={2}>
+      />
+      {/* <Stack spacing={2}>
       <Pagination count={postsPerPage} shape="rounded" />
       <Pagination count={10} variant="outlined" shape="rounded" />
-    </Stack>
+    </Stack> */}
     </div>
   );
 }
