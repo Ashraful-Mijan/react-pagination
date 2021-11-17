6435961e50b43c9d5db458366f5cb2470c302c7e
Ashraful-Mijan <ashrafulmijan5@gmail.com>
2021-11-17 15:37:16 +0600

    added material pagination

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import Posts from './components/Posts';
-import Pagination from './components/Pagination';
-// import Pagination from '@mui/material/Pagination';
-// import Stack from '@mui/material/Stack';
+// import Pagination from './components/Pagination';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
 export default function App() {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(10);
+  const [postsPerPage, setPostsPerPage] = useState(5);
   useEffect(() => {
     const fetchPosts = async () => {
       setLoading(true);
@@ -34,15 +34,28 @@ export default function App() {
       <h1 className="text-center text-primary mb-3">React Pagination!</h1>
 
       <Posts posts={currentPosts} loading={loading} />
-      <Pagination
+      {/* <Pagination
         postsPerPage={postsPerPage}
         totalPosts={posts.length}
         paginate={paginate}
+      /> */}
+      {/* <Pagination
+        count={Math.ceil(posts.length / postsPerPage)}
+        onClick={(e) => {
+          setCurrentPage(e.target.textContent);
+          console.log(e.target);
+        }}
+        shape="rounded"
+      /> */}
+      <Pagination
+        count={Math.ceil(posts.length / postsPerPage)}
+        onClick={(e) => {
+          setCurrentPage(e.target.textContent);
+          console.log(e);
+        }}
+        variant="outlined"
+        color="primary"
       />
-      {/* <Stack spacing={2}>
-      <Pagination count={postsPerPage} shape="rounded" />
-      <Pagination count={10} variant="outlined" shape="rounded" />
-    </Stack> */}
     </div>
   );
 }
